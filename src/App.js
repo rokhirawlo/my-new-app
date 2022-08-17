9f69cddf0f0b319de7987e41f70eaff537c75a6b
rokhirawlog10 <rokhi.rawlo@g10consultancy.com>
2022-08-17 10:11:57 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils';
+  
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
 
 function App() {
+
+   localStorage.setItem("user", JSON.stringify(person));
+
+   const  av =    JSON.parse(localStorage.getItem("user"));
+
+         console.log(av)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hello
+        <div className='new'>
+             im inside the new 
+
+        </div>
+
+
+        <button   >click me   </button>
+            
+    <div className='newname'>get the data</div>
+
+
     </div>
   );
 }
 
 export default App;
+
